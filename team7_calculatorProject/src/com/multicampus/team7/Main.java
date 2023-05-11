40a8d7cd1744209477651aa82a73a05c5cc4fe50
shj0212 <215212g@gmail.com>
2023-05-11 16:31:14 +0900

    프로젝트 frame

---
+++ team7_calculatorProject/src/com/multicampus/team7/Main.java
@@ -0,0 +1,8 @@
+package com.multicampus.team7;
+
+import com.multicampus.team7.service.AppService;
+
+public class Main {
+	AppService appservice = new AppService();
+	appservice.start();
+}
---
+++ team7_calculatorProject/src/com/multicampus/team7/service/AppService.java
@@ -0,0 +1,8 @@
+package com.multicampus.team7.service;
+
+public class AppService {
+
+	public void start() {
+		
+	}
+}
