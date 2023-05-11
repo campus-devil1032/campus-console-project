119137eeeb1a89f63c49c29ace9bdd108a48c9dc
startvision <nftgamereview@gmail.com>
2023-05-12 07:45:16 +0900

    test

--- src/MVCMain.java
+++ src/MVCMain.java
@@ -1,8 +1,11 @@
 import calculator.CalculatorController;
+import calculator.CalculatorView;
 
 public class MVCMain {
 public static void main(String[] args) {
 	CalculatorController ts = new CalculatorController();
+	CalculatorView ts2 = new CalculatorView();
 		ts.say();	
+		ts2.say2();
 }
 }
