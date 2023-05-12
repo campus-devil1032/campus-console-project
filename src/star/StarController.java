c3dc4e9534d7fb19290b540ff938fde15a271cc9
uxia <uxia@kakao.com>
2023-05-12 16:59:14 +0900

    test

--- src/star/StarController.java
+++ src/star/StarController.java
@@ -1,5 +1,21 @@
 package star;
 
 public class StarController {
-
+	
+	StarModel model = new StarModel();
+	StarView view = new StarView();
+			
+	public StarController(StarModel model, StarView view) {
+		// TODO Auto-generated constructor stub
+		
+		this.model = model;
+		this.view = view;
+		
+	}
+	
+	public void getSquareStar() {
+		this.model.setSquare();
+	}
+	
+	
 }
--- src/star/StarModel.java
+++ src/star/StarModel.java
@@ -2,4 +2,14 @@ package star;
 
 public class StarModel {
 
+	public void setSquare() {
+		//사각형 별모양 
+		System.out.println("별 찍기");
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("**********");
+		}
+		
+	}
+	
 }
