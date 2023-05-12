169fa2b24aee6bac2566daba22ce7564959a7476
5local <5local9805@gmail.com>
2023-05-12 14:05:03 +0900

    계산기 업로드

---
+++ calculator/Calculator.java
@@ -0,0 +1,71 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	Calculator calculator = new Calculator();
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+		
+			try {
+				
+				int num1; // 첫번째 입력 숫자
+				int num2; // 두 번째 입력 숫자
+				String operator; // 연산자
+				int result = 0; // 결과 값
+				
+				System.out.println("계산하실 첫 번째 숫자를 입력하세요.");
+				num1 = sc.nextInt();
+				
+				System.out.println("연산 기호를 입력하세요 (/, *, -, +, %)");
+				operator = sc.next();
+				
+				System.out.println("계산하실 두 번째 숫자를 입력하세요.");
+				num2 = sc.nextInt();
+				
+				if (operator.equals("/")) {
+					result = num1 / num2;
+				}
+				
+				else if (operator.equals("*")) {
+					result = num1 * num2;
+				}
+				
+				else if (operator.equals("-")) {
+					result = num1 - num2;
+				}
+				
+				else if (operator.equals("+")) {
+					result = num1 + num2;
+				}
+				
+				else if (operator.equals("%")) {
+					result = num1 % num2;
+				}
+				
+				else {
+					System.out.println("알 수 없는 연산자 입니다.");
+				}
+				
+				System.out.println(num1 + operator + num2 + "=" + result);
+			}
+			
+			catch (NullPointerException e) { // null 개체를 참조할 때
+				System.out.println(e.getMessage());
+			}
+
+			catch (NumberFormatException e) { // 문자열을 숫자로 변환할 수 없을 때
+				System.out.println(e.getMessage());
+			}
+			
+			catch (RuntimeException e) { // 런타임 중에 발생하는 예외
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
