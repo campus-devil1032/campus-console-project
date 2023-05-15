2c57e0a1de180f8fad392bf32ac80d05d6fdfbe5
Keitiankor <keitian.kor@gmail.com>
2023-05-15 17:46:46 +0900

    [Feat]별찍기 MVC모델

---
+++ Team9_Project1/Star/StarController.java
@@ -0,0 +1,33 @@
+
+public class StarController {
+	private StarView view;
+	private StarModel model;
+	
+	public StarController() {
+		this.view = new StarView();
+		this.model = new StarModel();
+	}
+	
+	public void showMenu() {
+		this.view.menuShow();
+		
+		switch(this.view.getMenuSelect()) {
+		case 1:
+			this.model.square(this.view.getSize());
+			break;
+		case 2:
+			this.model.upright(this.view.getSize());
+			break;
+		case 3:
+			this.model.triangle(this.view.getSize());
+			break;
+		case 4:
+			this.model.diamond(this.view.getSize());
+			break;
+		case 5:
+			this.model.starstar(this.view.getSize());
+			break;
+		}
+	}
+
+}
