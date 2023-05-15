8337bf448e88d08105e24dc589de958adc333115
teh4 <139567.naver.com>
2023-05-15 17:25:36 +0900

    0515 예시코드 삭제

--- 계산기구구단/ProView.java
+++
@@ -1,9 +0,0 @@
-package Program;
-
-public class ProView {
-	 void getInput() {
-
-		ProController controller = new ProController();
-		controller.run();
-	}
-}
--- 별찍기/사각형.js
+++
@@ -1,9 +0,0 @@
-let row = '';
-
-for (let i = 0; i < 10; i++){ 
-  for(let j = 0; j < 10; j++) {
-    row += '*';
-  }
-  row += '\n';
-}
-console.log(row);
