183eff6a57cc44ba396f01ae410f5b6c546858e5
Nodept <gupo941020@naver.com>
2023-05-16 16:20:15 +0900

    star파일 mainMVC로 합쳤습니다.

--- src/MVCMain.java
+++ src/MVCMain.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 import calculator.CalculatorController;
 import calculator.CalculatorModel;
 import calculator.CalculatorView;
 import gugudan.*;
+import star.StarMVCMain;
 //import뒤에 *를 붙이면 거기있는 모든 자바파일을 가져옵니다.
 
 public class MVCMain {
@@ -14,14 +16,16 @@ public class MVCMain {
 		CalculatorController calcController = new CalculatorController(calcModel, calcView);
 		// 구구단 선언부
 		GugudanView guguview = new GugudanView();
+		// 별찍기 선언부
+		StarMVCMain star = new StarMVCMain();
 		// 메인 선언부
 		boolean flag = true;
 
 		while (flag) {
-			System.out.println("0: 종료 "
-					+ "1: 구구단 "
-					+ "2: 계산기 "
-					+ "3: 별찍기");
+			System.out.println("0:종료  "
+					+ "1:구구단  "
+					+ "2:계산기  "
+					+ "3:별찍기 ");
 			Scanner sc = new Scanner(System.in);
 			int key = sc.nextInt();
 			switch (key) {
@@ -34,8 +38,10 @@ public class MVCMain {
 			case 2: // 계산기 실행
 				calcController.setIntro();
 				calcController.setCalcOperator();
+				calcController.clearArray();
 				break;
 			case 3: // 별찍기 실행
+				star.selcetStar();
 				break;
 			}
 
